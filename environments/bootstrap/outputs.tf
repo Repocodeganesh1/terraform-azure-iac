4cb220bde013980716c1263dbb53a028cc5b86a7
Ganesan Kasi <161068086+RepoCodeGanesh@users.noreply.github.com>
2026-08-01 17:38:46 +0530

    infra cost update

--- environments/bootstrap/outputs.tf
+++ environments/bootstrap/outputs.tf
@@ -1,24 +1,24 @@
 output "resource_group_name" {
   description = "Bootstrap resource group name."
-  value       = azurerm_resource_group.bootstrap.name
+  value       = azurerm_resource_group.bootstrap_new.name
 }
 
 output "storage_account_name" {
   description = "Terraform state storage account name."
-  value       = azurerm_storage_account.tfstate.name
+  value       = azurerm_storage_account.tfstate_new.name
 }
 
 output "storage_container_name" {
   description = "Terraform state container name."
-  value       = azurerm_storage_container.tfstate.name
+  value       = azurerm_storage_container.tfstate_new.name
 }
 
 output "key_vault_name" {
   description = "Bootstrap Key Vault name."
-  value       = module.key_vault.name
+  value       = module.key_vault_new.name
 }
 
 output "key_vault_uri" {
   description = "Bootstrap Key Vault URI."
-  value       = module.key_vault.vault_uri
+  value       = module.key_vault_new.vault_uri
 }
